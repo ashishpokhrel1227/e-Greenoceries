57a87a194bc31567f219606b76f576704b9cc20e
ashishpokhrel1227 <ashishpokhrel27@gmail.com>
2019-05-08 17:33:15 +0545

    errors seen in detail.html of cart is corrected

--- cart/cart.py
+++ cart/cart.py
@@ -13,7 +13,6 @@ class Cart(object):
         if not cart:
             #save an empty cart in the session
             cart = self.session[settings.CART_SESSION_ID] = {}
-
         self.cart = cart
     
     def add(self, product, quantity=1, update_quantity=False):
--- cart/views.py
+++ cart/views.py
@@ -17,7 +17,7 @@ def cart_add(request, product_id):
 def cart_remove(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
-    cart.remove()
+    cart.remove(product)
     return redirect('cart:cart_detail')
 
 def cart_detail(request):
