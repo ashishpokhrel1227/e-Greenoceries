89695afbe1820bf3e21b7e7e583792f8bd136910
ashishpokhrel1227 <ashishpokhrel27@gmail.com>
2019-05-10 19:15:26 +0545

    template has been added for ordering item

--- orders/views.py
+++ orders/views.py
@@ -10,12 +10,11 @@ def order_create(request):
         if form.is_valid():
             order = form.save()
             for item in cart: 
-                OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
-                
+                OrderItem.objects.create(order=order, product=item['product'], quantity=item['quantity'])  
             #clear the cart
             cart.clear()
             return render(request, 'orders/order/created.html', {'order': order})
 
-        else:
-            form = OrderCreateForm()
-        return render(request, 'orders/order/created.html', {'cart': cart, 'form': form})
+    else:
+        form = OrderCreateForm()
+    return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
