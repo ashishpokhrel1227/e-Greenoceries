cc5ba2526475291d256b368940e427d15c939eb4
ashishpokhrel1227 <ashishpokhrel27@gmail.com>
2019-05-16 08:48:16 +0545

    settings for asynchronous task using celery

--- myshop/settings.py
+++ myshop/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+EMAIL_BACKEND = 'django.core.mail.backend.console.EmailBackend'
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
